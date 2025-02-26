d1b94121635ddfb14643bed71daac8d341b7fe8d
Shivam Kumar <shivam@techarchsoftwares.in>
2025-02-26 15:52:15 +0530

    roles api done

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TeamController;
+use App\Http\Controllers\RoleController;
 
 
 // Route::get('/user', function (Request $request) {
@@ -21,6 +22,7 @@ Route::middleware('auth:api')->group(function () {
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
     Route::apiResource('/teams', TeamController::class);
+    Route::apiResource('/roles', RoleController::class);
 
 
     Route::post('/logout', [AuthController::class, 'logout']);
