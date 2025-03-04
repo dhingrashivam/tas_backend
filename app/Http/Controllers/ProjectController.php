04323588bafe9338c3228dabc451698a32c19267
Shivam Kumar <shivam@techarchsoftwares.in>
2025-03-04 16:30:07 +0530

    new changes add

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -17,7 +17,7 @@ class ProjectController extends Controller
     public function store(Request $request)
     {
         $validatedData = $request->validate([
-            'sales_team_id' => 'required|exists:teams,id',
+            'sales_team_id' => 'required',
             'client_id' => 'required|exists:clients,id',
             'project_name' => 'required|string|max:255',
             'requirements' => 'nullable|string',
